c9198268065b80faecd211cdf2bcad4f59b4ca63
Askar Pulatov <master15994@gmail.com>
2023-04-17 13:44:02 +0300

    finish part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  img =
-    'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-bleu.png';
-
-  constructor() {
-    setTimeout(() => {
-      this.img =
-        'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-vert.png';
-    }, 3000);
-  }
+  // objs = [
+  //   {
+  //     title: 'Post2',
+  //     author: 'Bek2',
+  //     comments: [
+  //       { name: 'Mike2', text: 'lorem2' },
+  //       { name: 'Mike2', text: 'lorem2' },
+  //       { name: 'Mike2', text: 'lorem2' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Post',
+  //     author: 'Bek1',
+  //     comments: [
+  //       { name: 'Mike', text: 'lorem1' },
+  //       { name: 'Mike', text: 'lorem1' },
+  //       { name: 'Mike', text: 'lorem1' },
+  //     ],
+  //   },
+  // ];
+  // array = [1, 1, 3, 4, 5, 6, 13];
+  // title = '';
+  // onInput(event:any) {
+  //   this.title = event.target.value;
+  // }
+  // inputValue = '';
+  // backgorundToggle1 = false;
+  // backgorundToggle = false;
+  // img =
+  //   'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-bleu.png';
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.img =
+  //       'https://icones.pro/wp-content/uploads/2022/07/icone-angulaire-vert.png';
+  //   }, 3000);
+  // }
+  // onInput(event?: any) {
+  //   this.inputValue = event.target.value;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
+
 import { Post2Component } from './post2/post2.component';
 import { Post4Component } from './post4.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component, Post4Component],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
